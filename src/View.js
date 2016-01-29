f37eebd64353ac91dcb68c6f81ef355e5e7b78b7
redwood <joseph.michael.sample@gmail.com>
2016-01-29 00:03:35 -0800

    comments etc

--- src/View.js
+++ src/View.js
@@ -2,10 +2,22 @@ import Backbone from 'backbone'
 
 const OtherViewClass = Backbone.View.extend()
 
-export default Backbone.View.extend({
+const ExportedClass = Backbone.View.extend({
+
+  /*
+   * Putting this other class on the prototype because this is how
+   * I realized the issue. When render method exists on ExportedClass
+   * creating a new instance of OtherViewClass should cause a TypeError
+   */
   OtherViewClass,
+
+  /*
+   * Uncomment the 'render' method below
+   */
 
   // render() {
   //
   // }
 })
+
+export default ExportedClass
--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import View from './View'
 
 const view = new View()
 
+/*
+ * Throws if View has a render method.
+ */
+
 new view.OtherViewClass()
